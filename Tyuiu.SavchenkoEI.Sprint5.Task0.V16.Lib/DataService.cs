ef4e210cb6d29e98ac5d43deb1a020f4e10a4c12
Pagarizantali <egor.igorevitch2002@yandex.ru>
2026-03-31 10:11:55 +0500

    Add project files.

---
+++ Tyuiu.SavchenkoEI.Sprint5.Task0.V16.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SavchenkoEI.Sprint5.Task0.V16.Lib
+{
+    public class DataService : ISprint5Task0V16
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask0.txt" });
+            double y = (2 * x * x - 1) / Math.Sqrt(x * x - 2);
+            y = Math.Round(y, 3);
+            File.WriteAllText(path, Convert.ToString(y));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.SavchenkoEI.Sprint5.Task0.V16.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.SavchenkoEI.Sprint5.Task0.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
